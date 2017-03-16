944123f2fb331a21b2df2a6e23853b9078b0d285
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-03-16 10:46:54 +0100

    feat: flask app with pandas to parse the csv file, format it correctly and send it as json to the template

---
+++ webapp/app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def index():
+    df = pd.DataFrame.from_csv('Result_top25.csv')
+    sub_df = df[['year', 'budgetPhase', 'Target', 'Score']]
+    sub_df.columns = ['x', 'c', 'y', 'size']
+
+    return render_template(
+        'index.html',
+        data=sub_df.to_json(orient='records')
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
