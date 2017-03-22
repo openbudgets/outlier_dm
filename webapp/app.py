d3a6d613a52909f4963e82c9435d538cd0e063bf
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-03-22 18:43:35 +0100

    feat: add list of files with statuses and links to details pages

--- webapp/app.py
+++ webapp/app.py
@@ -1,3 +1,6 @@
+import glob
+import os
+
 import pandas as pd
 from flask import Flask, render_template
 
@@ -5,15 +8,32 @@ app = Flask(__name__)
 app.debug = True
 
 
-@app.route('/outlier-dm-webapp')
-def index():
-    df = pd.DataFrame.from_csv('Result_top25.csv')
+@app.route('/outlier-dm-webapp/<path:filename>')
+def detail(filename):
+    print(filename)
+    df = pd.DataFrame.from_csv(filename)
     sub_df = df[['year', 'budgetPhase', 'Target', 'Score']]
     sub_df.columns = ['x', 'color', 'y', 'size']
 
     return render_template(
-        'index.html',
+        'detail.html',
         data=sub_df.to_json(orient='records')
+    )
+
+
+@app.route('/outlier-dm-webapp')
+def index():
+    files = []
+
+    for file in glob.glob('outputs/*.csv'):
+        if os.stat(file).st_size == 0:
+            files.append({"filename": file, "status": "Processing"})
+        else:
+            files.append({"filename": file, "status": "Ready"})
+
+    return render_template(
+        'list.html',
+        files=files
     )
 
 
