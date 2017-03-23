b1222496bfe3f12d25a4ef0d72989c856d6226a8
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-03-24 00:09:39 +0100

    feat: add API endpoints to retrieve the data

--- webapp/app.py
+++ webapp/app.py
@@ -2,40 +2,60 @@ import glob
 import os
 
 import pandas as pd
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 app.debug = True
 
 
-@app.route('/outlier-dm-webapp/<path:filename>')
-def detail(filename):
-    print(filename)
-    df = pd.DataFrame.from_csv(filename)
-    sub_df = df[['year', 'budgetPhase', 'Target', 'Score']]
-    sub_df.columns = ['x', 'color', 'y', 'size']
-
-    return render_template(
-        'detail.html',
-        data=sub_df.to_json(orient='records')
-    )
-
-
-@app.route('/outlier-dm-webapp')
-def index():
+def existing_files():
     files = []
-
     for file in glob.glob('outputs/*.csv'):
         if os.stat(file).st_size == 0:
             files.append({"filename": file, "status": "Processing"})
         else:
             files.append({"filename": file, "status": "Ready"})
+    return files
 
+
+def prepared_data(filename):
+    df = pd.DataFrame.from_csv(filename)
+    sub_df = df[['year', 'budgetPhase', 'Target', 'Score']]
+    sub_df.columns = ['x', 'color', 'y', 'size']
+    json_answer = sub_df.to_json(orient='records')
+    return json_answer
+
+
+@app.route('/outlier-dm-webapp/<path:filename>')
+def file_detail(filename):
+    json_answer = prepared_data(filename)
+
+    return render_template(
+        'detail.html',
+        data=json_answer
+    )
+
+
+@app.route('/outlier-dm-webapp')
+def index():
+    files = existing_files()
     return render_template(
         'list.html',
         files=files
     )
 
+
+@app.route('/outlier-dm-webapp/api/files')
+def api_files_list():
+    files = existing_files()
+    return jsonify(files)
+
+
+@app.route('/outlier-dm-webapp/api/files/<path:filename>')
+def api_file_detail(filename):
+    json_answer = prepared_data(filename)
+
+    return json_answer
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
